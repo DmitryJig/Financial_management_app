6c428a7936f1f079514d7321238144d91c02adcd
arcadag <piknik0711@yandex.ru>
2023-05-28 23:20:25 +0300

    implemented User, Role classes, implemented tasks #008, 012, 011

---
+++ src/main/java/com/finance/app/entity/Role.java
@@ -0,0 +1,17 @@
+package com.finance.app.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    private String roleName;
+}
