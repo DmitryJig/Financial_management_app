b6d2a6622ea196425486aa1684400257cb24c181
Konstantin8881888 <kamagoroff@gmail.com>
2023-05-29 10:34:50 +0300

    Structure
    Внесение  начального html. Изменение имени папки контролер на имя во множественном числе.

--- src/main/java/com/finance/app/controllers/HealthController.java
+++ src/main/java/com/finance/app/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package com.finance.app.controller;
+package com.finance.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
