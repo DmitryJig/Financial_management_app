fc1b117ac6b88d10a55eb687ca4c7b497a28f318
dima <jd2344430@gmail.com>
2023-06-04 21:29:24 +0500

    task_018 / create service for Profile class

--- src/main/java/com/finance/app/service/ProfileService.java
+++ src/main/java/com/finance/app/service/ProfileService.java
@@ -1,6 +1,7 @@
 package com.finance.app.service;
 
 import com.finance.app.entity.Profile;
+import com.finance.app.exception.ResourceNotFoundException;
 import com.finance.app.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,8 @@ public class ProfileService {
         return profileRepository.findAll();
     }
 
-    public Optional<Profile> findById(Long id){
-        return profileRepository.findById(id);
+    public Profile findById(Long id){
+        return profileRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException(String.format("Profile with id = %d not found", id)));
     }
 
     public Profile save(Profile profile){
