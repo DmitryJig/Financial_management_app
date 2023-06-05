3b041a05de911751622a79f6d6618d00706fa9d0
arcadag <piknik0711@yandex.ru>
2023-06-05 21:17:11 +0300

    fixed active profile in test

--- src/test/java/com/finance/app/service/UserDetailServiceImplTest.java
+++ src/test/java/com/finance/app/service/UserDetailServiceImplTest.java
@@ -8,9 +8,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AppApplication.class)
 @ActiveProfiles("prod")
