90f5ab87f07721c6c693031dcca9a713af7abd57
dima <jd2344430@gmail.com>
2023-06-06 20:48:33 +0500

    task_034 / create mapping user to dto

---
+++ src/main/java/com/finance/app/converters/UserConverter.java
@@ -0,0 +1,16 @@
+package com.finance.app.converters;
+
+import com.finance.app.dto.UserDto;
+import com.finance.app.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * Не стал делать лишних методов так как пока неизвестно в каких полях будет потребность
+ */
+@Component
+public class UserConverter {
+
+    public UserDto entityToDto(User user){
+        return new UserDto(user.getId(), user.getUsername(), user.getEmail());
+    }
+}
