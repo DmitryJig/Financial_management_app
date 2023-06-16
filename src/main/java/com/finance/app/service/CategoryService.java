ddd09197f2737339a48aaade2e30f6e324e6c45a
Stas <mccleric@ya.ru>
2023-06-16 17:52:43 +0300

    task 015 Создал контроллер для катерогий. Непольшие исправления в сущности, конвертере, сервисе. ! Если поле transactions не закоментированно, проверки в постмане не проходят. Не выяснил пока почему.

--- src/main/java/com/finance/app/service/CategoryService.java
+++ src/main/java/com/finance/app/service/CategoryService.java
@@ -1,7 +1,11 @@
 package com.finance.app.service;
 
+import com.finance.app.converters.CategoryConverter;
+import com.finance.app.exception.ResourceNotFoundException;
+import com.finance.app.model.dto.CategoryDto;
 import com.finance.app.model.entity.Category;
 import com.finance.app.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,28 +13,30 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
-    private final CategoryRepository categoryRepository;
 
-    @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
+    private final CategoryRepository categoryRepository;
+    private final CategoryConverter categoryConverter;
 
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
-    public Optional<Category> getCategoryById(Long id) {
-        return categoryRepository.findById(id);
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Объект не найден с таким ID " + id));
     }
 
-    public Optional<Category> getCategoryByTitle(String title) {
-        return categoryRepository.findByTitle(title);
+    public Category getCategoryByTitle(String title) {
+        return categoryRepository.findByTitle(title).orElseThrow(() ->
+                new ResourceNotFoundException("Объект не найден с таким названием " + title));
     }
 
-    public Category createOrUpdateCategory(Category category) {
-        return categoryRepository.save(category);
+    public CategoryDto createOrUpdateCategory(CategoryDto categoryDto) {
+        Category category = categoryConverter.dtoToEntity(categoryDto);
+        categoryRepository.save(category);
+        return categoryConverter.entityToDTO(category);
     }
 
     public void deleteCategoryById(Long id) {
