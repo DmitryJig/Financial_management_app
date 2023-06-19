450e011da219fb601411a9cb189244d154ff6461
FiataliS <FiataliS@gmail.com>
2023-06-19 22:58:27 +0500

    test controller complete now Закоментировал по моему мнению не нужные строки. Удаление юзера не должно происходить при удалении профиля.

--- src/main/java/com/finance/app/repository/ProfileRepository.java
+++ src/main/java/com/finance/app/repository/ProfileRepository.java
@@ -2,8 +2,6 @@ package com.finance.app.repository;
 
 import com.finance.app.model.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
