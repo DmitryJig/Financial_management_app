ecba62f1a2d800ba8260130f040e707b3565ead9
dima <jd2344430@gmail.com>
2023-06-22 22:43:28 +0500

    task_56/settings cors and create method findAllProfilesByUserId

--- src/main/java/com/finance/app/config/WebConfigCors.java
+++ src/main/java/com/finance/app/config/WebConfigCors.java
@@ -1,18 +1,22 @@
 package com.finance.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfigCors implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+public class WebConfigCors {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // разрешить CORS для всех путей
+                        .allowedOrigins("*") // разрешить запросы с любых доменов
+                        .allowedMethods("*") // разрешить использовать любые методы запросов
+                        .allowedHeaders("*"); // разрешить использовать любые заголовки запросов
+            }
+        };
     }
 }
