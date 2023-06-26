f150e310bcf38381be73730055f627fa925ab333
AlKulsha <alenabat18@gmail.com>
2023-06-26 18:58:12 +0300

    task_50 profile-category linking is completed

--- src/main/java/com/finance/app/model/entity/Category.java
+++ src/main/java/com/finance/app/model/entity/Category.java
@@ -28,4 +28,7 @@ public class Category {
     private String title;
     @OneToMany(mappedBy = "category")
     private Collection<Transaction> transactions;
+    @ManyToOne
+    @JoinColumn(name="profile_id")
+    private Profile profile;
 }
