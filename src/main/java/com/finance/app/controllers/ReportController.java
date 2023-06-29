64fca085a8b3874e12553e925e0257685df709ca
Konstantin8881888 <kamagoroff@gmail.com>
2023-06-29 17:19:44 +0300

    Update ReportController.java

--- src/main/java/com/finance/app/controllers/ReportController.java
+++ src/main/java/com/finance/app/controllers/ReportController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/users/{userId}/reports")
